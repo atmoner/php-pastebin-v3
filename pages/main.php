2c3c23d20fd983d3adee6ca2b444da02b1d769d9
Atmoner <contact@atmoner.com>
2013-02-08 22:05:26 +0100

    Fix captcha with function addJs()

--- pages/main.php
+++ pages/main.php
@@ -50,8 +50,7 @@ if (($_POST["txtCaptcha"] == $_SESSION["security_code"]) && (!empty($_POST["txtC
         } else {
         $smarty->assign("errorCaptcha",'1');
         }
- } 
- 
+}  
 
 $smarty->assign("getLangs",$startUp->getLangs()); 
  
@@ -59,7 +58,7 @@ $hook->set_title('home_title', $lang["titlehome"]);
 $hook->add_block('pasteForm', '', '',740,10); 
 $hook->add_block('pasteOptions', '', '',740,11);  
 $hook->add_block('pasteName', '', '',740,12);   
- 
+$hook->addJs('Captcha','ajax_captcha.js','libs/captcha/','5'); 
 
 if ($hook->hook_exist('home_page'))  
 	$hook->execute_hook('home_page');
