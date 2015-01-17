2000ed1b6157a1cc7ef469ae2e6c43a44cd6ce49
eagle00789 <info@decomputeur.nl>
2015-01-17 20:08:06 +0100

    create lang_nl.php (not all translated)
    Create lang_nl.php for the new dutch language (still work in progress)

---
+++ libs/lang/lang_nl.php
@@ -0,0 +1,99 @@
+<?php
+/*
+        __                                      
+_____ _/  |_  _____   ____   ____   ___________ 
+\__  \    __\/     \ /  _ \ /    \_/ __ \_  __ \
+ / __ \|  | |  Y Y  (  <_> )   |  \  ___/|  | \/
+(____  /__| |__|_|  /\____/|___|  /\___  >__|   
+     \/           \/            \/     \/     
+
+Website:  http://atmoner.com/
+Contact:  contact@atmoner.com     
+          
+*/
+
+$lang["Intro"]="Maak en deel je pastes snel";
+$lang["newPaste"]="Maak een nieuwe paste";
+$lang["viewPastes"]="Bekijk alle pastes";
+$lang["stats"]="Statistieken";
+$lang["Ops"]="Optionele Paste instellingen";
+$lang["syntax"]="Syntax";
+$lang["eXpiration"]="Vervaltijd";
+$lang["never"]="Nooit";
+$lang["10mn"]="10 Minuten";
+$lang["1h"]="1 uur";
+$lang["1d"]="1 Dag";
+$lang["1m"]="1 Maand";
+$lang["exposure"]="Publiciteit";
+$lang["public"]="Publiek";
+$lang["private"]="Prive";
+$lang["pasteName"]="Paste Naam";
+$lang["validcaptcha"]="Verifieer je Captcha";
+$lang["selectlang"]="Selecteer je taal";
+$lang["selectlife"]="Selecteer de levensduur van je paste";
+$lang["selectprivate"]="Maak je paste prive of publiek";
+$lang["recentPastes"]="Recente pastes";
+$lang["CoCtrit"]="Klik op de Captchaom hem te vernieuwen";
+$lang["changelang"]="Verander taal";
+$lang["english"]="Engels";
+$lang["french"]="Frans";
+$lang["russe"]="Russisch";
+$lang["dutch"]="Nederlands";
+$lang["Loginas"]="Ingelogd als";
+$lang["viewPaste"]="Bekijk paste";
+$lang["hits"]="Hits";
+$lang["accountSettings"]="Account Instellingen";
+$lang["logout"]="Afmelden";
+$lang["welcome"]="Welkom";
+$lang["registration"]="Registratie";
+$lang["login"]="Login";
+$lang["searchDynamic"]="Paste Search Dynamic";
+$lang["name"]="Name";
+$lang["added"]="Added";
+$lang["expires"]="Expires"; 
+$lang["syntax"]="Syntax"; 
+$lang["filterRecords"]="Filter records"; 
+$lang["display"]="Display"; 
+$lang["records"]="records"; 
+$lang["gotAtotal"]="Got a total of"; 
+$lang["entriesToshow"]="entries to show"; 
+$lang["close"]="Close"; 
+$lang["error"]="Error"; 
+$lang["nopaste"]="No paste, please added paste !!"; 
+$lang["wrongCaptcha"]="You have entered wrong captcha!!"; 
+$lang["closeStats"]="Close Stats"; 
+$lang["username"]="Username"; 
+$lang["mail"]="mail"; 
+$lang["password"]="Password"; 
+$lang["author"]="Author"; 
+$lang["userInfo"]="User info"; 
+$lang["userName"]="User name"; 
+$lang["statut"]="Statut"; 
+$lang["userdetailof"]="Detail of"; 
+$lang["userInfo"]="User info";
+$lang["userSigna"]="User signature";
+$lang["userloc"]="Location";
+$lang["userweb"]="Website";
+$lang["allpasteof"]="All pastes of";
+$lang["editInfo"]="Edit your information";
+$lang["avatarset"]="Avatar settings";
+$lang["mailsettings"]="Mail settings";
+$lang["showmailconf"]="Show your email on your profile public.";
+$lang["notshowmailconf"]="Not show your email on your profile public.";
+$lang["Submit"]="Submit";
+$lang["editProfile"]="Edit profil";
+$lang["Search"]="Search";
+
+//Page title
+$lang["titleacount"]="Your account";
+$lang["titlehome"]="Add paste";
+$lang["titlelastpaste"]="Last paste";
+$lang["titleEdit"]="Edit account";
+
+// Menu
+$lang["adminsettings"]="Admin settings";
+$lang["adminusers"]="Users management";
+$lang["adminpastes"]="Pastes management";
+$lang["adminplugins"]="Plugins management";
+
+
